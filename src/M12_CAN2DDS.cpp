ed508b398a51bace4695f0290cce7475683e2a35
ahonena <andrei.ahonen@tuni.fi>
2020-03-06 16:37:31 +0200

    Removed the interpretation of numbers, now only transporting as raw as possible

--- src/M12_CAN2DDS.cpp
+++ src/M12_CAN2DDS.cpp
@@ -81,7 +81,9 @@ void M12_CAN2DDS::listen_and_publish(){
   dds::topic::Topic<M12_input_and_hsd> M12_input_and_hsd_topic(participant_m12, "Driver_input");
   dds::topic::Topic<M12_Resolver> M12_Resolver_topic(participant_m12, "Resolver");
   dds::topic::Topic<M12_IMU_gyro> M12_IMU_gyro_topic(participant_m12, "IMU_gyro");
+
   dds::topic::Topic<M12_RawCAN> M12_RawCAN_topic(participant_m12, "RawCAN");
+
   dds::topic::Topic<M12_EMstop> M12_EMstop_topic(participant_m12, "EMstop_status");
   dds::topic::Topic<M12_WorkHydraulicPosition> M12_WorkHydraulicPosition_topic(participant_m12, "WorkHydraulicPositions");
   dds::topic::Topic<M12_WorkHydraulicPressures> M12_WorkHydraulicPressures_topic(participant_m12, "WorkHydraulicPressures");
@@ -92,7 +94,9 @@ void M12_CAN2DDS::listen_and_publish(){
   dds::pub::DataWriter<M12_input_and_hsd> writer_M12_input_and_hsd_topic(publisher_M12, M12_input_and_hsd_topic);
   dds::pub::DataWriter<M12_Resolver> writer_M12_Resolver_topic(publisher_M12, M12_Resolver_topic);
   dds::pub::DataWriter<M12_IMU_gyro> writer_M12_IMU_gyro_topic(publisher_M12, M12_IMU_gyro_topic);
+
   dds::pub::DataWriter<M12_RawCAN> writer_M12_RawCAN_topic(publisher_M12, M12_RawCAN_topic);
+
   dds::pub::DataWriter<M12_EMstop> writer_M12_EMstop_topic(publisher_M12, M12_EMstop_topic);
   dds::pub::DataWriter<M12_WorkHydraulicPosition> writer_M12_WorkHydraulicPosition_topic(publisher_M12, M12_WorkHydraulicPosition_topic);
   dds::pub::DataWriter<M12_WorkHydraulicPressures> writer_M12_WorkHydraulicPressures_topic(publisher_M12, M12_WorkHydraulicPressures_topic);
@@ -153,6 +157,7 @@ void M12_CAN2DDS::listen_and_publish(){
       // IMU acceleration
 
       case 0x2B2:{
+        /*
         double g_ = 9.82; // m/s² per 1 g
         double g_per_unit = 0.25*0.001;// From ADIS 16385 documentation
         double acc_per_unit = g_*g_per_unit;
@@ -175,8 +180,16 @@ void M12_CAN2DDS::listen_and_publish(){
         double acc_x = double (unit_acceleration_x)*acc_per_unit;
         double acc_y = double (unit_acceleration_y)*acc_per_unit;
         double acc_z = double (unit_acceleration_z)*acc_per_unit;
+        */
+        int16_t acc_x = 0;
+        int16_t acc_y = 0;
+        int16_t acc_z = 0;
+        memcpy(&acc_x, Message.DATA + 0,2);
+        memcpy(&acc_y, Message.DATA + 2,2);
+        memcpy(&acc_z, Message.DATA + 4,2);
 
         M12_IMU_acceleration imu_acc_sample;
+
         imu_acc_sample.x_acceleration(acc_x);
         imu_acc_sample.y_acceleration(acc_y);
         imu_acc_sample.z_acceleration(acc_z);
@@ -190,11 +203,14 @@ void M12_CAN2DDS::listen_and_publish(){
 
       // Input and HSD
       case 0x485:{
-
+        /*
         uint8_t byte_0 = Message.DATA[0];
         uint8_t byte_1 = Message.DATA[1];
         uint unit_motor_speed = uint (byte_0 << 8 | byte_1);
         double motor_speed = 10.0*double (unit_motor_speed);
+        */
+        uint16_t motor_speed = 0;
+        memcpy(&motor_speed, Message.DATA + 0, 2);
 
         hsd_direction_enum motor_direction;
         uint8_t byte_2 = Message.DATA[2];
@@ -236,7 +252,7 @@ void M12_CAN2DDS::listen_and_publish(){
       // Resolver
       case 0x19F:{
         std::cout << "Resolver message received..." << std::endl;
-
+        /*
         int16_t bytes_0_1 = int16_t (Message.DATA[0] << 8 | Message.DATA[1]);
         double resolver_angle = static_cast<double>(bytes_0_1);
         resolver_angle = resolver_angle*0.01; // from 0.01 deg/unit  to 1 deg/unit
@@ -250,16 +266,26 @@ void M12_CAN2DDS::listen_and_publish(){
         M12_Resolver resolver_sample;
         resolver_sample.position(resolver_angle);
         resolver_sample.velocity(resolver_velocity);
+        */
+        int16_t resolver_angle = 0;
+        memcpy(&resolver_angle, Message.DATA + 0, 2);
 
-        std::cout << "Computed resolver angle in radians: " <<  resolver_angle <<  std::endl;
-        std::cout << "Computed resolver velocity in radians per second: " <<  resolver_velocity <<  std::endl;
+        int32_t resolver_velocity = 0;
+        memcpy(&resolver_velocity, Message.DATA + 4, 4);
+
+        M12_Resolver resolver_sample;
+        resolver_sample.position(resolver_angle);
+        resolver_sample.velocity(resolver_velocity);
+
+        //std::cout << "Computed resolver angle in radians: " <<  resolver_angle <<  std::endl;
+        //std::cout << "Computed resolver velocity in radians per second: " <<  resolver_velocity <<  std::endl;
         writer_M12_Resolver_topic.write(resolver_sample);
         break;
       }
 
       case 0x1B2:{
         std::cout << "IMU gyro message received..." << std::endl;
-
+        /*
         int16_t angular_velocity_x = int16_t (Message.DATA[0] << 8 | Message.DATA[1]);
         int16_t angular_velocity_y = int16_t (Message.DATA[2] << 8 | Message.DATA[3]);
         int16_t angular_velocity_z = int16_t (Message.DATA[4] << 8 | Message.DATA[5]);
@@ -279,6 +305,20 @@ void M12_CAN2DDS::listen_and_publish(){
 
         double chiptmp = static_cast<double>(temperature);
         chiptmp = 25.0 + 0.0678*chiptmp;
+
+        M12_IMU_gyro imu_gyro_sample;
+        */
+        int16_t angvx = 0;
+        memcpy(&angvx, Message.DATA + 0, 2);
+
+        int16_t angvy = 0;
+        memcpy(&angvy, Message.DATA + 2, 2);
+
+        int16_t angvz = 0;
+        memcpy(&angvz, Message.DATA + 4, 2);
+
+        int16_t chiptmp = 0;
+        memcpy(&chiptmp, Message.DATA + 6, 2);
 
         M12_IMU_gyro imu_gyro_sample;
         imu_gyro_sample.x_rate(angvx);
@@ -311,18 +351,26 @@ void M12_CAN2DDS::listen_and_publish(){
 
       case 0x186:{
         std::cout << "Work hydraulic actuator positions message received..." << std::endl;
+        /*
         int16_t liftpos = int16_t(Message.DATA[0] << 8 | Message.DATA[1]);
         int16_t tiltpos = int16_t(Message.DATA[2] << 8 | Message.DATA[3]);
+        */
+        int16_t liftpos = 0;
+        memcpy(&liftpos, Message.DATA + 0,2);
+
+        int16_t tiltpos = 0;
+        memcpy(&tiltpos, Message.DATA + 2,2);
+
         M12_WorkHydraulicPosition workpos_sample;
-        workpos_sample.lift(static_cast<double>(liftpos));
-        workpos_sample.tilt(static_cast<double>(tiltpos));
+        workpos_sample.lift(liftpos);
+        workpos_sample.tilt(tiltpos);
         writer_M12_WorkHydraulicPosition_topic.write(workpos_sample);
         break;
       }
 
       case 0x189:{
         std::cout << "Hydraulic pressures message received..." << std::endl;
-
+        /*
         uint16_t pA_lift = uint16_t(Message.DATA[0] << 8 | Message.DATA[1]);
         uint16_t pB_lift = uint16_t(Message.DATA[2] << 8 | Message.DATA[3]);
         uint16_t pA_tilt = uint16_t(Message.DATA[4] << 8 | Message.DATA[5]);
@@ -332,6 +380,18 @@ void M12_CAN2DDS::listen_and_publish(){
         double lift_cylB_pressure = 0.01*static_cast<double>(pB_lift);
         double tilt_cylA_pressure = 0.01*static_cast<double>(pA_tilt);
         double tilt_cylB_pressure = 0.01*static_cast<double>(pB_tilt);
+        */
+        uint16_t lift_cylA_pressure = 0;
+        memcpy(&lift_cylA_pressure, Message.DATA + 0, 2);
+
+        uint16_t lift_cylB_pressure = 0;
+        memcpy(&lift_cylB_pressure, Message.DATA + 2, 2);
+
+        uint16_t tilt_cylA_pressure = 0;
+        memcpy(&tilt_cylA_pressure, Message.DATA + 4, 2);
+
+        uint16_t tilt_cylB_pressure = 0;
+        memcpy(&tilt_cylB_pressure, Message.DATA + 6, 2);
 
         M12_WorkHydraulicPressures workpres_sample;
         workpres_sample.lift_cylA_pressure(lift_cylA_pressure);
